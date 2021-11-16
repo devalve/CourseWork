aa37ebae1985d17213349d1063eeca9a46c96267
devalve <sekiro.shad@gmail.com>
2021-11-16 14:56:21 +0400

    Добавьте файлы проекта.

---
+++ Model/Data/AppContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork.Model.Data
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<User> User { get; set; }
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=CourseWork;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork.Model
+{
+    public class User
+    {
+        public int Id{ get; set; }
+        public string Nickname{ get; set; }
+        public string Password{ get; set; }
+    }
+}
