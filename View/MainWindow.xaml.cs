04873842f20115b53d2463f1aa1e0bb1c56d2793
devalve <sekiro.shad@gmail.com>
2021-11-16 15:01:36 +0400

    migrations + structurization

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CourseWork
+namespace CourseWork.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
