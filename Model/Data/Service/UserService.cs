0300715a845b6dc344ee3f80d04ee53c72e8a8c9
devalve <sekiro.shad@gmail.com>
2021-12-10 11:07:42 +0400

    add user data to analog of HashMap - Dictionary<TKey, TValue>

--- Model/Data/Service/UserService.cs
+++ Model/Data/Service/UserService.cs
@@ -1,13 +1,19 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace CourseWork.Model.Data.Service
 {
     public class UserService
     {
+
+        readonly static Dictionary<int, User> users = new();
         public static bool AuthUser(string nickname, string password)
         {
             using (ApplicationContext db = new())
             {
+                foreach (var item in db.Users)
+                    users.Add(item.Id, item);
+
                 bool isAuthenticated = db.Users.Any(u => u.Nickname == nickname & u.Password == password);
                 if (isAuthenticated) return true;
             }
