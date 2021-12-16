45b70a8e9767fd2b85975a3a91c76011765dc2d3
devalve <sekiro.shad@gmail.com>
2021-12-16 11:35:56 +0400

    create test user data on loaded

--- Model/Data/Service/UserService.cs
+++ Model/Data/Service/UserService.cs
@@ -12,13 +12,19 @@ namespace CourseWork.Model.Data.Service
             using (ApplicationContext db = new())
             {
                 foreach (var item in db.Users)
-                    users.Add(item.Id, item);
+                    if (!users.ContainsKey(item.Id)) users.Add(item.Id, item);
 
                 bool isAuthenticated = db.Users.Any(u => u.Nickname == nickname & u.Password == password);
                 if (isAuthenticated) return true;
             }
             return false;
-
+        }
+        public static void CreateUser(string nickname, string password)
+        {
+            using ApplicationContext db = new();
+            bool isAuthenticated = db.Users.Any(u => u.Nickname == nickname & u.Password == password);
+            if (!isAuthenticated) db.Users.Add(new User() { Nickname = nickname, Password = password });
+            db.SaveChanges();
         }
     }
 }
